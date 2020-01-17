b1751ed5d5e47a4eab0a6e50f533437450a60335
Allison Bradley <allison.bradley@outlook.com>
2020-01-16 18:53:57 -0700

    Added PDF capability

--- assets/index.js
+++ assets/index.js
@@ -3,6 +3,7 @@ const axios = require("axios")
 const inquirer = require("inquirer")
 const util = require("util")
 const generateHTML = require("./generateHTML")
+const pdf = require('html-pdf');
 
 const writeFileAsync = util.promisify(fs.writeFile)
 
@@ -47,6 +48,13 @@ function promptUser() {
                 const html = generateHTML.makeMyHTML(apiResponse, color, stars)
                 writeFileAsync(`${githubUser}.html`, html).then(function() {
                     console.log(`"Successfully wrote to ${githubUser}.html file"`);
+                    const file = fs.readFileSync(`./${githubUser}.html`, 'utf8')
+                    var options = { format: 'Tabloid'};
+ 
+                    pdf.create(html, options).toFile(`./${githubUser}.pdf`, function(err, res) {
+                        if (err) return console.log(err);
+                        console.log(res); // { filename: '/app/businesscard.pdf' }
+                    });
                   });
         })
     })
